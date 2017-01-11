b33889d971af7aa933191b3fed6b285f04d4625f
James Gallicchio <jamesgallicchio@gmail.com>
2017-01-11 11:31:16 -0500

    Rewrite of Omni Controls
    - Removes modify()
    - Adds getOmniPowers()
    - Fixes updateOmni() accordingly
    - Removes messy work for things >.>

--- main.c
+++ main.c
@@ -1,14 +1,3 @@
-// Modifies the xyz vectors so that x+y+z <= 127 to make for more accurate controls
-int * modify(int x, int y, int z) {
-    float hypo = sqrt(pow(x, 2) + pow(y, 2));
-    float rotPer = z/127.0;
-    int nums[3];
-    nums[0] = (1 - rotPer) * x * hypo/127;  // formula for x
-    nums[1] = (1 - rotPer) * y * hypo/127;  // formula for y
-    nums[2] = rotPer * z;
-    return nums;
-}
-
 // Limits n to between -limit and limit
 int lim(int n, int limit)
 {
@@ -25,20 +14,57 @@ int curve(int n)
     return pow(lim(n, 127), 3)/16129;
 }
 
+// Takes xyz vectors and creates the correct omni powers for it
+/*
+Calculate vector as (A, d) where 0<=A<8, 0<=d<127
+if(floor(A/2)):
+	0= -1, A-1, 1, 1-A
+	1= A-3, 1, 3-A, -1
+	2= 1, 5-A, -1, A-5
+	3= 7-A, -1, A-7, 1
+
+multiply all by d
+*/
+int * getOmniPowers(int x, int y, int z) {
+    int d = curve(sqrt(pow(x, 2) + pow(x, 2)));
+    float A = (1.27323954474 * atan2(y, x)+8)%8;
+    int powers[4];
+    switch(floor(A/2)) {
+    	case 0:
+    		powers[0] = -d;
+    		powers[1] = d*(A-1);
+    		powers[2] = d;
+    		powers[3] = d*(1-A);
+    		break;
+    	case 1:
+    		powers[0] = d*(A-3);
+    		powers[1] = d;
+    		powers[2] = d*(3-A);
+    		powers[3] = -d;
+    		break;
+    	case 2:
+    		powers[0] = d;
+    		powers[1] = d*(5-A);
+    		powers[2] = -d;
+    		powers[3] = d*(A-5);
+    		break;
+    	case 3:
+    		powers[0] = d*(7-A);
+    		powers[1] = -d;
+    		powers[2] = d*(A-7);
+    		powers[3] = d;
+    }
+    return powers;
+}
+
 // Updates the power of the wheels
 void updateOmni()
 {
-    /*int * coords = modify(vexRT[Ch4], vexRT[Ch3], vexRT[Ch1]);
-    int x = coords[0];
-    int y = coords[1];
-    int z = coords[2];*/
-    int x = vexRT[Ch4];
-    int y = vexRT[Ch3];
-    int z = vexRT[Ch1];
-    motor[port2] = curve(x+y+z);
-    motor[port3] = curve(x-y+z);
-    motor[port4] = curve(-x-y+z);
-    motor[port5] = curve(-x+y+z);
+    int * powers = getOmniPowers(vexRT[Ch4], vexRT[Ch3], vexRT[Ch1]);
+    motor[port2] = powers[0];
+    motor[port3] = powers[1];
+    motor[port4] = powers[2];
+    motor[port5] = powers[3];
 }
 
 // When robot starts, this runs
@@ -48,16 +74,3 @@ task main()
         updateOmni();
     }
 }
-/*
-0/1   -1  1  1 -1		-1  1  1 -1
-1/1   -1  0  1  0		-2  0  2  0
-1/0   -1 -1  1  1		-1 -1  1  1
-1/-1   0 -1  0  1		 0 -2  0  1
-0/-1   1 -1 -1  1
--1/-1  1  0 -1  0
--1/0   1  1 -1 -1
--1/1   0  1  0 -1
-0/0    0  0  0  0
-
--x-y  -x+y  x+y  x-y
-/(|x|+|y|)*/
